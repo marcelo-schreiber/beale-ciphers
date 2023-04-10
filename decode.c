adca840f0fde57d68eeb1d6c275d9a3dde63bcb9
marcelo-schreiber-dinf <marceloschreiber@ufpr.br>
2023-04-10 19:07:58 -0300

    Feat: generate numbers using keycode file

--- decode.c
+++ decode.c
@@ -82,6 +82,93 @@ void decodeGenerateFileUsingCodesKeys(char *numbers_cypher, const char *filename
   fclose(fp);
 }
 
+char *decodeGenerateNumbersUsingCodeskeys(const char *filename)
+{
+  FILE *fp = fopen(filename, "r");
+
+  if (fp == NULL)
+  {
+    printf("Error opening file");
+    exit(1);
+  }
+
+  // a: 15 31 35 48 59 63 65 72 85 89
+  // c: 10 32 41 42 68 70 79
+  // d: 7 18 20 33 36 46 56 58 61 64 67 80 83 86
+  // e: 0 37 40 51 53 62 77 96
+  // f: 6 17 24 25 28 69
+  // i: 3 30 47 88
+  // o: 2 13 22 39 93 98
+  // 1: 1 38
+  // ...
+
+  // transform into
+  // 0: e
+  // 1: 1
+  // 2: o
+  // ...
+
+  char *numbers_cypher = malloc(sizeof(char));
+  numbers_cypher[0] = ' ';
+
+  if (numbers_cypher == NULL)
+  {
+    printf("Error allocating memory");
+    exit(1);
+  }
+
+  char letter;
+
+  char line[255];
+  int biggest_num = -1;
+  char *token = malloc(sizeof(char) * 10);
+
+  while (fgets(line, 255, fp) != NULL)
+  {
+    letter = line[0];
+    token = strtok(line, " ");
+
+    while (token != NULL)
+    {
+
+      if (strlen(token) == 2 && token[1] == ':')
+      {
+        token = strtok(NULL, " ");
+        continue;
+      }
+      if (strlen(token) == 1 && token[0] == '\n')
+      {
+        token = strtok(NULL, " ");
+        continue;
+      }
+
+      int number = atoi(token);
+
+      if (number > biggest_num)
+      {
+        numbers_cypher = realloc(numbers_cypher, sizeof(char) * (number + 1));
+        biggest_num = number;
+        if (numbers_cypher == NULL)
+        {
+          printf("Error allocating memory");
+          exit(1);
+        }
+      }
+
+      numbers_cypher[number] = letter;
+
+      token = strtok(NULL, " ");
+    }
+  }
+
+  numbers_cypher[biggest_num + 1] = '\0';
+
+  free(token);
+  fclose(fp);
+
+  return numbers_cypher;
+}
+
 void decodeGenerateFileUsingCypherBook(FILE *cypher_book, const char *filename, FILE *encoded_message_file)
 {
 
--- decode.h
+++ decode.h
@@ -4,3 +4,4 @@
 char *decodeGenerateNumbers(FILE *fp);
 void decodeGenerateFileUsingCodesKeys(char *numbers_cypher, const char *filename, FILE *encoded_message_file);
 void decodeGenerateFileUsingCypherBook(FILE *cypher_book, const char *filename, FILE *encoded_message_file);
+char *decodeGenerateNumbersUsingCodeskeys(const char *filename);
