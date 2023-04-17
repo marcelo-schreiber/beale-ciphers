4610b4b932513cb54ea1cedd7d87acfac35d52c0
marcelo-schreiber-dinf <marceloschreiber@ufpr.br>
2023-04-17 17:45:34 -0300

    Feat: make functions inline for performance

--- src/utils.c
+++ src/utils.c
@@ -1,14 +1,14 @@
 #include <stdlib.h>
 #include "../include/utils.h"
 
-char arrIdxToChar(int code)
+inline char arrIdxToChar(int code)
 {
   if (code < 0 || code > NUM_OF_CHARS - 1)
     return -1;
   return code + FIRST_CHAR;
 }
 
-int charToArrIdx(char c)
+inline int charToArrIdx(char c)
 {
   if (c < FIRST_CHAR || c > LAST_CHAR)
     return -1;
