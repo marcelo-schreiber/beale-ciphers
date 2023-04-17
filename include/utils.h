1cad0e162520f06d22ea4a546207cb89b9424c1d
marcelo-schreiber <maraugrissch@gmail.com>
2023-04-17 00:17:45 -0300

    Feat: accept new characters to encode and decode

--- include/utils.h
+++ include/utils.h
@@ -1,7 +1,9 @@
 #include <stdlib.h>
 
-#define NUM_OF_CHARS 26 + 10 // 26 letters and 10 numbers
-#define MAX_WORD_SIZE 255    // 255 characters
+#define NUM_OF_CHARS 93 // from 33 to 126 (33 to 126 - 33 = 93) (from ! to ~)
+#define FIRST_CHAR '!'
+#define LAST_CHAR '~'
+#define MAX_WORD_SIZE 255 // 255 characters
 
 char arrIdxToChar(int code);
 int charToArrIdx(char c);
--- src/utils.c
+++ src/utils.c
@@ -3,20 +3,14 @@
 
 char arrIdxToChar(int code)
 {
-  if (code < 26)
-    return code + 'a';
-  if (code >= 26 && code < 36)
-    return code + '0' - 26;
-  else
+  if (code < 0 || code > NUM_OF_CHARS - 1)
     return -1;
+  return code + '!';
 }
 
 int charToArrIdx(char c)
 {
-  if (c >= 'a' && c <= 'z')
-    return c - 'a';
-  if (c >= '0' && c <= '9')
-    return c - '0' + 26;
-  else
+  if (c < FIRST_CHAR || c > LAST_CHAR)
     return -1;
+  return c - '!';
 }
